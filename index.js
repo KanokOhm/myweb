8c7e8e7c581b95db28b88dc0339e662b37422a61
KanokOhm <oome288@gmail.com>
2024-10-16 00:37:31 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,272 @@
+var product = [{
+    id: 1,
+    img: 'https://www.newbestshop.com/best/wp-content/uploads/images36/wimg/url04/unknown/1146949232-4091_1.jpeg',
+    name: 'WILSON Clash 100 V2.0',
+    price: 6300,
+    description: 'ไม้เทนนิส WILSON Clash 100 V2.0 ได้รับการออกแบบมาอย่างพิถีพิถันเพื่อมอบความสม่ำเสมอและความยั่งยืนที่มากขึ้น ไม้เทนนิสนี้ผสมผสานความยืดหยุ่นอันยอดเยี่ยมเข้ากับความมั่นคงที่น่าทึ่ง ส่งมอบความรู้สึกที่เป็นเอกลักษณ์ที่ช่วยเพิ่มประสิทธิภาพในการเล่นของคุณบนสนาม ด้วยโครงสร้างเฟรมที่ปรับปรุงใหม่ซึ่งขยายจุด Sweet Spot จึงให้พลังและการควบคุมที่ดีที่สุดสำหรับทุกการสวิง',
+    type: 'equipment'
+}, {
+    id: 2,
+    img: 'https://static.supersports.co.th/BABOLAT-BABOLATSateliteGravity78UnstrungBadmintonRacket-SSP63600865-1',
+    name: 'BABOLAT Satelite Gravity 78 Unstrung',
+    description: 'ไม้แบดมินตัน BABOLAT Satelite Gravity 78 Unstrung ถูกออกแบบมาเพื่อมอบความคล่องตัวและความแม่นยำที่ไม่เหมือนใคร เหมาะสำหรับผู้เล่นที่ต้องการการควบคุมที่ดีพร้อมทั้งพลังในการตีที่สูง ไม้รุ่นนี้มาพร้อมกับการออกแบบที่ทันสมัยและน้ำหนักเบา ช่วยให้คุณเคลื่อนไหวได้อย่างรวดเร็วในสนาม',
+    price: 4580,
+    type: 'equipment'
+}, {
+    id: 3,
+    img: 'https://static.supersports.co.th/ADIDAS-AD001AC263DTTH-SSP66307600-2?$JPEG$&wid=550',
+    name: 'ลูกฟุตบอล UCL League 23/24 Knockout',
+    description: 'ลูกฟุตบอล UCL League 23/24 Knockout เป็นลูกฟุตบอลที่ออกแบบมาเพื่อการแข่งขันยูฟ่าแชมเปียนส์ลีก (UCL) ในฤดูกาล 2023/24 ให้ความรู้สึกในการเล่นที่ดีที่สุดและความทนทานสูงสุดในสนาม',
+    price: 840,
+    type: 'equipment'
+}, {
+    id: 4,
+    img: 'https://static.supersports.co.th/MOLTEN-OrangeBasketballBallB7G3800Size7FIBAApproved-MKP1669547-1?$JPEG$&wid=550  ',
+    name: ' ลูกบาส MOLTEN',
+    description: 'ลูกบาส MOLTEN ได้รับการออกแบบมาเพื่อความทนทานและการควบคุมที่ยอดเยี่ยม ทั้งในการแข่งขันและการฝึกซ้อม',
+    price: 2000,
+    type: 'equipment'
+}, {
+    id: 5,
+    img: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/f1fcf220e5ac4648979d1d9cf46ce470_9366/Anthony_Edwards_1_WITH_LOVE_IF1859_01_standard.jpg',
+    name: 'รองเท้า Anthony Edwards 1 WITH LOVE',
+    description: 'Anthony Edwards 1 WITH LOVE',
+    price: 4500,
+    type: 'shoe'
+}, {
+    id: 6,
+    img: 'https://tse4.mm.bing.net/th?id=OIP.Gx8A2L4e23sdlC_QWpi7qQHaHa&pid=Api&P=0&h=220',
+    name: 'รองเท้า ASICS CYBERBLADE HF',
+    description: 'รองเท้า ASICS CYBERBLADE HF ได้รับการออกแบบมาเพื่อให้ความสะดวกสบายและการสนับสนุนที่ยอดเยี่ยมในการออกกำลังกายและกีฬาต่าง ๆ',
+    price: 3800,
+    type: 'shoe'
+}, {
+    id: 7,
+    img: 'https://uptodate.tokyo/wp-content/uploads/2024/01/IMG_6958.jpeg',
+    name: 'LeBron NXXT Gen AMPD EP',
+    description: 'LeBron NXXT Gen AMPD EP ได้รับการออกแบบมาเพื่อมอบความสบายและประสิทธิภาพที่ดีที่สุดสำหรับผู้เล่นบาสเก็ตบอลทุกระดับ พร้อมด้วยเทคโนโลยีทันสมัยที่ช่วยสนับสนุนการเคลื่อนไหวอย่างคล่องตัวในสนาม',
+    price: 5900,
+    type: 'shoe'
+}, {
+    id: 8,
+    img: 'https://images.asics.com/is/image/asics/1051A086_750_SR_RT_GLB?$sfcc-product$',
+    name: 'NETBURNER BALLISTIC FF MT 3 PARIS',
+    description: 'ETBURNER BALLISTIC FF MT 3 PARIS คือรองเท้าสำหรับนักกีฬาที่มอบความสบายและประสิทธิภาพสูงสุดในสนาม ด้วยการออกแบบที่เน้นการรองรับแรงกระแทกและความยืดหยุ่นในการเคลื่อนไหว',
+    price: 5300,
+    type: 'shoe'
+}, {
+    id: 9,
+    img: 'https://i.ebayimg.com/images/g/QSUAAOSwsfdmloIN/s-l1600.webp',
+    name: 'Nike Mercurial Superfly 10 Academy',
+    description: 'Nike Mercurial Superfly 10 Academy เป็นรองเท้าฟุตบอลที่ออกแบบมาเพื่อมอบความเร็วและความคล่องตัวในสนาม ด้วยการใช้เทคโนโลยีที่ทันสมัยเพื่อช่วยให้คุณเคลื่อนที่ได้อย่างรวดเร็วและมั่นใจ',
+    price: 3200,
+    type: 'shoe'
+}, {
+    id: 10,
+    img: 'https://assets.adidas.com/images/h_2000,f_auto,q_auto,fl_lossy,c_fill,g_auto/0bd72582c09e4378afbb560470e60b28_faec/Fear_of_God_Athletics_IW1482_HM3_hover.tiff.jpg',
+    name: 'กางเกงวิ่งรัดรูปเบสเลเยอร์สองส่วน Fear of God Athletics',
+    description: 'กางเกงวิ่งรัดรูปเบสเลเยอร์สองส่วนจาก Fear of God Athletics ออกแบบมาเพื่อมอบความสะดวกสบายและการสนับสนุนที่ดีที่สุดในระหว่างการวิ่ง ด้วยวัสดุที่มีคุณภาพสูงและการออกแบบที่ทันสมัย',
+    price: 4500,
+    type: 'shirt'
+}, {
+    id: 11,
+    img: 'https://www.vereinsexpress.de/Data/Images/Big/10156365-nike-tech-fleece-windrunner-blau-f410.jpg',
+    name: 'Nike Tech Windrunner',
+    description: 'Nike Tech Windrunner เป็นแจ็คเก็ตกันลมที่ออกแบบมาเพื่อมอบความสะดวกสบายและการเคลื่อนไหวที่มีประสิทธิภาพในการออกกำลังกายของคุณ ด้วยการออกแบบที่ทันสมัยและฟังก์ชันที่หลากหลาย ทำให้เหมาะสำหรับการใช้งานทั้งในและนอกสนาม',
+    price: 3200,
+    type: 'shirt'
+}, {
+    id: 12,
+    img: 'https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/79d2cede-230e-4742-8254-f39d80b9cfe5/GSW+MNK+DF+SWGMN+JSY+HWC+24.png',
+    name: 'BaskeStephen Curry Golden State Warriors 2024/25 Hardwood Classicstball',
+    description: 'เสื้อบาสเกตบอล BaskeStephen Curry Golden State Warriors 2024/25 Hardwood Classics เป็นเสื้อที่ออกแบบมาเพื่อเฉลิมฉลองประวัติศาสตร์ของทีม Golden State Warriors และความสำเร็จของ Stephen Curry ในสนาม',
+    price: 4500,
+    type: 'shirt'
+}, {
+    id: 13,
+    img: 'https://www.tfcfootball.com.my/wp-content/uploads/2021/05/DB2560-688-1.jpg',
+    name: 'LFC Nike Mens 24/25 Home Stadium Jersey',
+    description: 'เสื้อแข่ง LFC Nike ผู้ชาย 24/25 (เหย้า) ผสมผสานระหว่างการใช้งานและสไตล์สำหรับแฟน ๆ ลิเวอร์พูล เอฟซี เสื้อรุ่นนี้ออกแบบด้วยผ้าทางเทคโนโลยีที่ช่วยระบายอากาศ ทำให้คุณรู้สึกสบายตลอดทั้งวัน ไม่ว่าจะใส่ในชีวิตประจำวันหรือในวันที่มีการแข่งขัน เสื้อมีสีแดงอันเป็นสัญลักษณ์ของมรดกของทีม ขณะที่ตราสัญลักษณ์ของลิเวอร์พูล เอฟซี แสดงถึงการสนับสนุนของคุณ เสื้อรุ่นนี้มีรูปทรงทันสมัย แผงระบายอากาศที่ช่วยให้มีการไหลเวียนของอากาศ และเทคโนโลยี Dri-FIT จากไนกี้เพื่อให้รู้สึกเบาสบาย เหมาะสำหรับทั้งนักกีฬาและแฟนบอล แสดงออกถึงความรักในทีมของคุณด้วยเสื้อแข่งชิ้นนี้',
+    price: 3330,
+    type: 'shirt'
+}, {
+    id: 14,
+    img: 'https://shop-image.readyplanet.com/cwLVvcGgZZzVc7xXTtzRipYKIbI=/4253aab842864cf691d47bab5329ca9e',
+    name: 'ลูกปิงปอง Butterfly Easy Ball 40+(1x6)',
+    description: 'ลูกปิงปอง Butterfly Easy Ball 40+ มีการออกแบบที่เป็นเอกลักษณ์เพื่อให้ความรู้สึกที่ดีในขณะเล่น พร้อมกับการควบคุมที่แม่นยำ',
+    price: 540 - 720,
+    type: 'equipment'
+}, {
+    id: 15,
+    img: 'https://store.liverpoolfc.com/media/catalog/product/cache/6e0c7b53c0ed72fe014b8d12b60d479c/2/3/23.07_pet0382_1.jpg',
+    name: 'Basketเสื้อยืดสัตว์เลี้ยง LFC 24/25 ทีมเหย้าball',
+    description: 'High-quality basketball for professionals and beginners.',
+    price: 1200,
+    type: 'shirt'
+}];
+
+$(document).ready(() => {
+    var html = '';
+    for (let i = 0; i < product.length; i++) {
+
+        html += `
+            <div onclick="openProductDetail(${i})" class="product-items ${product[i].type}">
+                <img class="product-img" src="${product[i].img}" alt=""/>
+                <p style="font-size: 1.2vw;">${product[i].name}</p>
+                <p style="font-size: 1vw;">${numberWithCommas(product[i].price)} THB</p>
+            </div>`;
+    }
+    $("#productlist").html(html);
+});
+
+function numberWithCommas(x) {
+    x = x.toString();
+    var pattern = /(-?\d+)(\d{3})/;
+    while (pattern.test(x))
+        x = x.replace(pattern, "$1,$2");
+    return x;
+}
+
+function searchsomething(elem) {
+    var value = $('#' + elem.id).val();
+    var html = '';
+    for (let i = 0; i < product.length; i++) {
+        if (product[i].name.toLowerCase().includes(value.toLowerCase())) {
+            html += `<div onclick="openProductDetail(${i})" class="product-items ${product[i].type}">
+                <img class="product-img" src="${product[i].img}" alt=""/>
+                <p style="font-size: 1.2vw;">${product[i].name}</p>
+                <p style="font-size: 1vw;">${numberWithCommas(product[i].price)} THB</p>
+            </div>`;
+        }
+    }
+    if (html === '') {
+        $("#productlist").html('<p>Not found product</p>');
+    } else {
+        $("#productlist").html(html);
+    }
+}
+
+function searchproduct(param) {
+    $(".product-items").css('display', 'none'); // ซ่อนสินค้าทั้งหมดก่อน
+    if (param === 'all') {
+        $(".product-items").css('display', 'block'); // แสดงสินค้าทั้งหมด
+    } else {
+        $("." + param).css('display', 'block'); // แสดงเฉพาะสินค้าตามหมวดหมู่
+    }
+}
+
+var productindex = 0;
+
+function openProductDetail(index) {
+    productindex = index;
+    $("#modalDesc").css('display', 'flex');
+    $("#mdd-img").attr('src', product[index].img);
+    $("#mdd-name").text(product[index].name);
+    $("#mdd-price").text(numberWithCommas(product[index].price) + ' THB');
+    $("#mdd-desc").text(product[index].description);
+}
+
+function closeModal() {
+    $(".modal").css('display', 'none');
+}
+
+var cart = [];
+
+function addtocart() {
+    var pass = true;
+
+    for (let i = 0; i < cart.length; i++) {
+        if (productindex === cart[i].index) {
+            cart[i].count++;
+            pass = false;
+        }
+    }
+
+    if (pass) {
+        var obj = {
+            index: productindex,
+            id: product[productindex].id,
+            name: product[productindex].name,
+            price: product[productindex].price,
+            img: product[productindex].img,
+            count: 1
+        };
+        cart.push(obj);
+    }
+    Swal.fire({
+        title: `Add ${product[productindex].name} to cart!`,
+        icon: 'success',
+        showConfirmButton: false,
+        timer: 1500
+    });
+    $("#cartcount").css('display', 'flex').text(cart.length);
+}
+
+function openCart() {
+    $('#modalCart').css('display', 'flex');
+    rendercart();
+}
+
+function rendercart() {
+    if (cart.length > 0) {
+        var html = '';
+        for (let i = 0; i < cart.length; i++) {
+            html += `<div class="cartlist-items">
+                <div class="cartlist-left">
+                    <img src="${cart[i].img}" alt="">
+                    <div class="cartlist-detail">
+                        <p style="font-size: 1.5vw;">${cart[i].name}</p>
+                        <p id="cartlist-price${i}" style="font-size: 1.2vw;">${numberWithCommas(cart[i].price * cart[i].count)} THB</p>
+                    </div>
+                </div>
+                <div class="cartlist-right">
+                    <p onclick="deinitems('-', ${i})" class="btnc">-</p>
+                    <p id="countitems${i}" style="margin: 0 20px;">${cart[i].count}</p>
+                    <p onclick="deinitems('+', ${i})" class="btnc">+</p>
+                </div>
+            </div>`;
+        }
+        $("#mycart").html(html);
+    } else {
+        $("#mycart").html('<p>Not found product list</p>');
+    }
+}
+
+function deinitems(action, index) {
+    if (action === '-') {
+        if (cart[index].count > 0) {
+            cart[index].count--;
+            $("#countitems" + index).text(cart[index].count);
+
+            if (cart[index].count === 0) {
+                Swal.fire({
+                    icon: 'warning',
+                    title: 'Are you sure to delete?',
+                    showConfirmButton: true,
+                    showCancelButton: true,
+                    confirmButtonText: 'Delete',
+                    cancelButtonText: 'Cancel'
+                }).then((res) => {
+                    if (res.isConfirmed) {
+                        cart.splice(index, 1);
+                        rendercart();
+                        $("#cartcount").css('display', 'flex').text(cart.length);
+                        if (cart.length <= 0) {
+                            $("#cartcount").css('display', 'none');
+                        }
+                    } else {
+                        cart[index].count++;
+                        $("#countitems" + index).text(cart[index].count);
+                    }
+                });
+            } else {
+                // อัปเดตราคาเมื่อจำนวนสินค้าถูกเปลี่ยน
+                $("#cartlist-price" + index).text(numberWithCommas(cart[index].price * cart[index].count));
+            }
+        }
+    } else if (action === '+') {
+        cart[index].count++;
+        $("#countitems" + index).text(cart[index].count);
+        $("#cartlist-price" + index).text(numberWithCommas(cart[index].price * cart[index].count));
+    }
+}
